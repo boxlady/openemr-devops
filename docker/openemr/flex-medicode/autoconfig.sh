664474753f34ae36c30e0fcdaa2a1ec9b235bcfe
julie <boxlady@gmail.com>
2020-04-29 16:28:06 +0200

    2 fixed certbot formating

--- docker/openemr/flex-medicode/autoconfig.sh
+++ docker/openemr/flex-medicode/autoconfig.sh
@@ -122,7 +122,8 @@ if [ -f /etc/docker-leader ] ||
             /usr/sbin/httpd -k start
             sleep 2
             #certbot certonly --webroot -n -w /var/www/localhost/htdocs/openemr/ -d $DOMAIN $EMAIL --agree-tos
-            certbot certonly --webroot -n -w /var/www/localhost/htdocs/openemr/ -d www.medicode.nl, medicode.nl -m boxlady@gmail.com --agree-tos
+            #certbot certonly --webroot -n -w /var/www/localhost/htdocs/openemr/ -d www.medicode.nl --agree-tos
+            certbot certonly --webroot --agree-tos -w /var/www/localhost/htdocs/openemr/ \--expand -d medicode.nl,www.medicode.nl,openemr.medicode.nl
             /usr/sbin/httpd -k stop
             echo "1 23  *   *   *   certbot renew -q --post-hook \"httpd -k graceful\"" >> /etc/crontabs/root
         fi
